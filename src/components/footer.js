932c816a3de4c1116abcbd68c08994490070a56c
Athena Vassilopoulos <athena.vassilo@gmail.com>
2020-09-19 19:04:20 +0200

    fixed width of elements, spaces between icons, text is fixed, videos centered' '

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,31 +7,34 @@ import SVG from "react-inlinesvg"
 const Footer = props => {
   return (
     <div className="footer">
-      <div className="allicons">
-        <a
-          href="https://ehess.academia.edu/AnnaTheodorides"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <SVG src={iconac} />
-        </a>
-        <a
-          href="https://www.facebook.com/lespetitesconfinees"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <SVG src={iconfb} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/anna-theodorides-639816bb/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <SVG src={iconlk} />
-        </a>
+      <div className="alliconswrapper">
+        {" "}
+        <div className="allicons">
+          <a
+            href="https://ehess.academia.edu/AnnaTheodorides"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <SVG src={iconac} />
+          </a>
+          <a
+            href="https://www.facebook.com/lespetitesconfinees"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <SVG src={iconfb} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/anna-theodorides-639816bb/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <SVG src={iconlk} />
+          </a>
+        </div>
       </div>
     </div>
   )
